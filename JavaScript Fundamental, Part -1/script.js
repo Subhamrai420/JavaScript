7ca9c42ccee299174824dcb70bbb02e45df13407
Subham <raisubham1024@gmail.com>
2023-02-19 11:00:09 +0530

    fundamental part 2 added

---
+++ JavaScript Fundamental, Part -1/script.js
@@ -0,0 +1,170 @@
+/*
+// challenge 1
+
+const mark_weight = 95
+const mark_height = 1.88
+
+const john_weight = 85
+const john_height = 1.76
+
+// BMI = mass/ height ** 2
+
+const mark_BMI = mark_weight / mark_height ** 2
+const john_BMI = john_weight / john_height ** 2
+
+const markHigherBMI = mark_BMI > john_BMI
+
+console.log("Mark BMI is Higher than John BMI : " + markHigherBMI)
+*/
+
+// const age = 12
+// const isOldEnough = age >= 18
+
+// if (isOldEnough) {
+//   console.log(`Subham can start driving license 🚗`)
+// } else {
+//   const yearLeft = 18 - age
+//   console.log(`Subham is too young, wait another ${yearLeft} year :)`)
+// }
+
+// const birthYear = 165
+
+// // if (birthYear <= 2000) {
+// //   console.log(`You are in 19th century`)
+// // } else {
+// //   console.log(`You are in 20th century`)
+// // }
+
+// let century
+
+// if (birthYear <= 2000) {
+//   century = 20
+// } else {
+//   century = 21
+// }
+
+// console.log(`You are in ${century}th century`)
+
+// const mark_weight = 78
+// const mark_height = 1.69
+
+// const john_weight = 92
+// const john_height = 1.95
+
+// // BMI = mass/ height ** 2
+
+// const mark_BMI = mark_weight / mark_height ** 2
+// const john_BMI = john_weight / john_height ** 2
+
+// if (mark_BMI > john_BMI) {
+//   console.log(`Mark's BMI(${mark_BMI}) is higher than John's(${john_BMI})!`)
+// } else {
+//   console.log(`John's(${john_BMI}) BMI is higher than Mark's(${mark_BMI})!`)
+// }
+
+// console.log(Boolean(0))
+// console.log(Boolean(undefined))
+// console.log(Boolean("Subham"))
+// console.log(Boolean({}))
+
+// const money = 0
+
+// if (money) {
+//   console.log(`Don't spend it all ;)`)
+// } else {
+//   console.log(`You should get a job!`)
+// }
+
+// let height = 0
+
+// if (height || height == 0) {
+//   console.log(`YAY! Height is defined`)
+// } else {
+//   console.log(`Height is undefined`)
+// }
+
+// // challenge #3
+// const dolphinScore_1 = 97
+// const dolphinScore_2 = 112
+// const dolphinScore_3 = 101
+
+// const koalaScore_1 = 109
+// const koalaScore_2 = 95
+// const koalaScore_3 = 106
+
+// const avg_of_dolphin = (dolphinScore_1 + dolphinScore_2 + dolphinScore_3) / 3
+// const avg_of_koala = (koalaScore_1 + koalaScore_2 + koalaScore_3) / 3
+
+// console.log(avg_of_dolphin)
+// console.log(avg_of_koala)
+
+// if (avg_of_dolphin > avg_of_koala && avg_of_dolphin >= 100) {
+//   console.log(`Team Dolphin is winner 🏆!`)
+// } else if (avg_of_koala > avg_of_dolphin && avg_of_koala >= 100) {
+//   console.log(`Team Koala is winner 🏆!`)
+// } else if (avg_of_dolphin === avg_of_koala && avg_of_dolphin >= 100) {
+//   console.log(`Both win the Trophy 🏆✌️`)
+// } else {
+//   console.log(`No one cover 100 score
+// Both team loose!!`)
+// }
+
+// const day = "friday"
+
+// switch (day) {
+//   case "monday":
+//     console.log(`Plan course structure`)
+//     console.log(`Go to coding meetup`)
+//     break
+
+//   case "tuesday":
+//     console.log(`Prepare theory videos`)
+//     break
+
+//   case "wednesday":
+//   case "thursday":
+//     console.log(`Write code examples`)
+//     break
+
+//   case "friday":
+//     console.log(`Record videos`)
+//     break
+
+//   case "saturday":
+//   case "sunday":
+//     console.log(`Enjoy your Time 😊`)
+// }
+
+// const user_input = Number(prompt("Choose between 1 to 5 : "))
+
+// switch (user_input) {
+//   case 1:
+//   case 2:
+//   case 4:
+//   case 5:
+//     console.log(`Your choice is Awesome dude 😉!`)
+//     break
+
+//   case 3:
+//     console.log(`Your choice is Shit 💩😂`)
+//     break
+
+//   default:
+//     console.log(`Please choose number between 1 to 5`)
+// }
+
+// const age = 5
+
+// console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💦"}`)
+
+// challenge #4
+
+// const billValue = 430
+// const tip =
+//   billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.2
+
+// console.log(
+//   `The bill was ${billValue}, the tip was ${tip} and the totoal value ${
+//     billValue + tip
+//   }`
+// )
